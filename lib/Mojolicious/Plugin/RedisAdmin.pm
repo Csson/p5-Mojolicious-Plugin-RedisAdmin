5e9984283e365a0a27e382a6f0905c5b5d29b02d
Erik Carlsson <info@code301.com>
2015-11-20 12:22:56 +0000

    Init

---
+++ lib/Mojolicious/Plugin/RedisAdmin.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package Mojolicious::Plugin::RedisAdmin;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Mojolicious::Plugin::RedisAdmin;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::RedisAdmin is ...
+
+=head1 SEE ALSO
+
+=cut
